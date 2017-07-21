18e98d5cd3e2a095ed0e5be774932321b838ada8
= <zachary31@outlook.com>
2017-07-21 20:05:29 +0800

    StringApI测试类

--- src/char03/StringAPI.java
+++ src/char03/StringAPI.java
@@ -5,4 +5,31 @@ package char03;
  *         Created by zacky on 19:39.
  */
 public class StringAPI {
+    public static void main(String[] args) {
+        String str = "Coding is a funny thing!";
+        System.out.println(str.charAt(5));
+        System.out.println(str.codePointAt(5));
+        System.out.println(str.offsetByCodePoints(5,5));
+        System.out.println(str.compareTo("Coding"));
+        System.out.println(str.endsWith("thing!"));
+        System.out.println(str.equals("Coding is a funny thing!"));
+        System.out.println(str.equalsIgnoreCase("CODING IS A FUNNY THING!"));
+        System.out.println(str.indexOf("f"));
+        System.out.println(str.indexOf("f",5));
+        System.out.println(str.indexOf(5));
+        System.out.println(str.indexOf(5,5));
+        System.out.println(str.lastIndexOf("ing"));
+        System.out.println(str.lastIndexOf("ing",5));
+        System.out.println(str.lastIndexOf(5));
+        System.out.println(str.lastIndexOf(5,5));
+        System.out.println(str.length());
+        System.out.println(str.codePointCount(0,20));
+        System.out.println(str.replace("ing","e"));
+        System.out.println(str.startsWith("Cod"));
+        System.out.println(str.substring(1));
+        System.out.println(str.substring(1,10));
+        System.out.println(str.toLowerCase());
+        System.out.println(str.toUpperCase());
+        System.out.println(str.trim());
+    }
 }
