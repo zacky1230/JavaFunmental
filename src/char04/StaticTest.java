c40ece984219497bb1a36583dc9bb3e17bbe872d
= <zachary31@outlook.com>
2017-08-03 20:01:54 +0800

    面向对象基础

---
+++ src/char04/StaticTest.java
@@ -0,0 +1,20 @@
+package char04;
+
+/**
+ * @author gy1zc3@gmail.com
+ *         Created by zacky on 23:03.
+ */
+public class StaticTest {
+    public static void main(String[] args) {
+        Employee[] staff = new Employee[3];
+        staff[0] = new Employee("Tom",40000);
+        staff[1] = new Employee("Dick",60000);
+        staff[2] = new Employee("Harry", 65000);
+        for(Employee e: staff){
+            e.setId();
+            System.out.println("name="+e.getName() + ",id="+e.getId()+",salary="+e.getSalary());
+        }
+        int n = Employee.getNextId();
+        System.out.println("Next available id=" + n);
+    }
+}
---
+++ src/char04/Test.java
@@ -0,0 +1,14 @@
+package char04;
+
+import java.util.Date;
+import java.util.GregorianCalendar;
+
+/**
+ * @author gy1zc3@gmail.com
+ *         Created by zacky on 20:31.
+ */
+public class Test {
+    public static void main(String[] args) {
+        Date date = new Date();
+    }
+}
